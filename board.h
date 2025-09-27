de6b1ef90b6075d17fe047d1a6ff5b7497d79cde
drdmch <dariademchik07@gmail.com>
2025-09-27 18:41:27 +0300

    Update board.h

--- board.h
+++ board.h
@@ -1,31 +1,24 @@
 #pragma once
 
-#include "randomCell.h"
 #include <vector>
-//#include <utility>
+#include <utility>
 
-class Board {
-public:
-	explicit Board(int size, int cell);
-
-	std::size_t free_zone_size(const std::vector<std::pair<int, int>>& selected) const; //int adj = boardSize
-
-	double mean(const std::vector <std::size_t>& v) const;
-	double median(std::vector<std::size_t> v) const;
-
-	void experiment(int exp, double& average, double& median);
-
+class Board{
 private:
-	//int boardSize;
-	int boardSize;
-	int count_selected_cell;
+    int boardSize;
 
-	void validate_parameters() const;
-	bool is_neighbor_cell(int r, int c, const std::pair<int, int>& selected) const;
-	bool is_valid_cell(int r, int c) const;
+    void mark_neighbors(std::vector<unsigned char>& blocked, int r, int c) const;
+    void validate_parameters() const;
+    bool is_valid_cell(int r, int c) const;
+public:
+    explicit Board(int n);
 
-	void mark_blocked_all_sides(std::vector<unsigned char>& blocked, int r, int c) const;
-	void mark_blocked_vertical_horizontal(std::vector<unsigned char>& blocked, int r, int c) const;
+    std::size_t free_zone_size(const std::vector<std::pair<int, int>>& selected) const;
 
+    double mean(const std::vector < std::size_t>& v) const;
+    double median(std::vector<std::size_t> v) const;
 
+    int size() const {
+        return boardSize;
+    };
 };
