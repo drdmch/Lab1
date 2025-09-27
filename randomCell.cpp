c8b355afc95ea89e09ffe32c4c96457b8134f5b8
drdmch <dariademchik07@gmail.com>
2025-09-27 18:44:54 +0300

    Update randomCell.cpp

--- randomCell.cpp
+++ randomCell.cpp
@@ -1,30 +1,22 @@
 #include "randomCell.h"
 #include <stdexcept>
+#include <random>
 
-RandomCell::RandomCell(int n) : boardSize(n), distribution(0, n - 1) {
+RandomCell::RandomCell(int n) : boardSize(n), distribution(0, n*n - 1) {
     if (boardSize <= 0) {
         throw std::invalid_argument("Board size must be positive.");
-    }
-    if (boardSize > 1000) {
-        throw std::invalid_argument("Board size exceeds the maximum allowed size of 1000."); //під питанням!!!! ВИПРАВИЛА
     }
     std::random_device rand;
     generator = std::mt19937(rand());
     }
 
 std::pair<int, int> RandomCell::operator()() const {
-   
-    if (board_size == 1) {
-        return { 0, 0 };
-    }
-    int row = distribution(generator);
-    int col = distribution(generator);
+    int cellIndex = distribution(generator);
+    int row = cellIndex / boardSize;
+    int col = cellIndex % boardSize;
     return {row, col};
 }
 
 int RandomCell::getSize() const {
     return boardSize;
 }
-
-
-
